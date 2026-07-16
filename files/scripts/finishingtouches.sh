9b3a843a6608216be04dea010cf927da187c2c68
Emi <120683059+emberaine@users.noreply.github.com>
2026-07-15 22:12:12 -0700

    fix login screen colors FINALLY

--- files/scripts/finishingtouches.sh
+++ files/scripts/finishingtouches.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
-# rm -f /usr/share/applications/nvim.desktop
+rm -f /usr/share/applications/nvim.desktop
 rm -f /usr/share/applications/btop.desktop
 rm -f /usr/share/applications/nvtop.desktop
 rm -f /usr/share/applications/htop.desktop
+
+dconf update
