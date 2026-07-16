f43dfe1ca39081d8dffcd8d87903ec6c89b8bcb2
Emi <120683059+emberaine@users.noreply.github.com>
2026-07-15 17:02:56 -0700

    configure fish somewhat, still need to make default blackbox config

--- files/scripts/cleanup.sh
+++ files/scripts/cleanup.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-rm -f /usr/share/applications/nvim.desktop
+# rm -f /usr/share/applications/nvim.desktop
 rm -f /usr/share/applications/btop.desktop
 rm -f /usr/share/applications/nvtop.desktop
 rm -f /usr/share/applications/htop.desktop
